0356cb73687ee8aeec73d00b12233d733ab79042
jhida <jhida@DESKTOP-7N4Q6SN>
2021-11-08 15:06:21 +0200

    Add project files.

---
+++ AwesomeSause.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class AwesomeSause
+    {
+        public List<string> Sauces { get; set; }
+
+        public AwesomeSause()
+        {
+            Sauces = new List<string>();
+        }
+
+        public bool IsSauceAwesome(string sauce)
+        {
+            return Sauces.Contains(sauce);
+        }
+
+    }
+}
---
+++ FlowControl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining
+{
+    public class FlowControl
+    {
+        public string PrimaryOrSecondary(string color)
+        {
+            var result = "";
+            if(color.ToLower() == "red")
+            {
+                result = "Primary";
+            }    
+            //elseif..... statements followed. 
+        }
+        public bool IsYourFavoriteColorYellow(string color)
+        {
+            return (color.ToLower() == "yellow");
+        }
+        public bool IsYourFavoriteColorGreen(string color)
+        {
+            return (color.ToLower() == "green") ? true : false;
+        }
+        public bool IsYourFavoriteColorRed(string color)
+        {
+            if (color.ToLower() == "red") return true;
+            return false;
+        }
+        public bool IsYourFavoriteColorBlue(string color)
+        {
+            if(color.ToLower() == "blue")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
